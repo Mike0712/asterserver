69f441fff28931421a8d8dd965f5354b3e23f6ac
Mikhail Churikov <barrister2003@gmail.com>
2025-11-04 13:16:42 +0000

    fix

--- src/routes/add.ts
+++ src/routes/add.ts
@@ -24,7 +24,7 @@ addRouter.post('/', async (req: Request, res: Response) => {
     );
 
     await pool.query(
-      'INSERT INTO ps_endpoints (id, aors, auth, context, disallow, allow, direct_media, webrtc, media_encryption_optimistic, force_rport, rtp_symmetric, rewrite_contact, dtls_cert_file, dtls_private_key) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+      'INSERT INTO ps_endpoints (id, aors, auth, context, disallow, allow, direct_media, webrtc, media_encryption_optimistic, force_rport, rtp_symmetric, rewrite_contact, dtls_cert_file, dtls_private_key) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
       [id, id, id, 'ari_context', 'all', 'ulaw,alaw', 'no', 'yes', 'yes', 'yes', 'yes', 'yes', config.dtls_cert_file, config.dtls_private_key]
     );
 
